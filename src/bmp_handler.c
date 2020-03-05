1e2ba8bfb9183e5d5159af796a6cbc21f6722ddb
Paulo Ricardo J. Miranda <pauloricardojm@hotmail.com.br>
2020-03-04 22:28:31 -0300

    Added BMP_FILE data type

--- src/bmp_handler.c
+++ src/bmp_handler.c
@@ -1,6 +1,6 @@
 #include <bmp_handler.h>
 
-struct t_bmp_info_header
+typedef struct t_bmp_info_header
 {
 	unsigned int bmpHeaderSize;
     unsigned int bmpWidth;
@@ -13,9 +13,9 @@ struct t_bmp_info_header
     unsigned int bmpImportantColors;
     unsigned short bmpBitsPerPixel;
     unsigned short bmpPlanes;
-};
+} BMP_INFO_HEADER;
 
-struct t_bmp_header
+typedef struct t_bmp_header
 {
 	unsigned int bmpFileSize; // Size of BMP file in bytes
 	unsigned int bmpPixelDataOffset; // Offset to the pixel data
@@ -23,10 +23,15 @@ struct t_bmp_header
 	unsigned short bmpReserverd1; // Reserved for application
 	unsigned short bmpReserver2; // Reserved for application
 	struct t_bmp_info_header info_header; // Rest of BMP Header
-};
+} BMP_CHANNELS;
 
 struct t_bmp_channels
 {
     char **r, **g, **b;
 };
 
+struct t_bmp_file
+{
+    struct t_bmp_info_header;
+    struct t_bmp_channels;
+};
