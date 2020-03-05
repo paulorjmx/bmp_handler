a7569a0adb82916557b1aa2e3609a3e01fd8aa44
Paulo Ricardo J. Miranda <pauloricardojm@hotmail.com.br>
2020-03-04 22:28:42 -0300

    Added some functions signatures

--- inc/bmp_handler.h
+++ inc/bmp_handler.h
@@ -2,5 +2,8 @@
 	#define BMP_HANDLER_H
 
 		typedef struct t_bmp_channels BMP_CHANNELS;
-		typedef struct t_bmp_header BMP_INFO_HEADER;
+		typedef struct t_bmp_file BMP_FILE;
+
+		void bmp_read_file(const char *file_name);
+		BMP_CHANNELS *bmp_get_channels();
 #endif
