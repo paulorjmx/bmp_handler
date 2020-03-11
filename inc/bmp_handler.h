e5cc998eba8b003fd8bd1d8680886c56a4b805dd
Paulo Ricardo J. Miranda <pauloricardojm@hotmail.com.br>
2020-03-11 16:53:43 -0300

    Change parameter style on sig functions

--- inc/bmp_handler.h
+++ inc/bmp_handler.h
@@ -4,7 +4,8 @@
 		typedef struct t_bmp_channels BMP_CHANNELS;
 		typedef struct t_bmp_file BMP_FILE;
 
-		BMP_FILE *bmp_read_file(const char *file_name);
+		BMP_FILE *bmp_read_file(const char *);
+		int bmp_write_file(const char *, BMP_FILE *);
 		BMP_CHANNELS *bmp_get_channels();
-		void bmp_destroy(BMP_FILE **bmp);
+		void bmp_destroy(BMP_FILE **);
 #endif
