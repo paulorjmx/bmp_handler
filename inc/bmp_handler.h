145bcbc395a16368fba7572df7200c121fb5ad17
Paulo Ricardo J. Miranda <pauloricardojm@hotmail.com.br>
2020-03-11 16:16:46 -0300

    Added destroy bmp image function

--- inc/bmp_handler.h
+++ inc/bmp_handler.h
@@ -4,6 +4,7 @@
 		typedef struct t_bmp_channels BMP_CHANNELS;
 		typedef struct t_bmp_file BMP_FILE;
 
-		void bmp_read_file(const char *file_name);
+		BMP_FILE *bmp_read_file(const char *file_name);
 		BMP_CHANNELS *bmp_get_channels();
+		void bmp_destroy(BMP_FILE **bmp);
 #endif
