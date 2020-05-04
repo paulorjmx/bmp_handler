59ea21d5233da73c646c3264d1f141073b584bb3
Paulo Ricard J. Miranda <pauloricardojm@hotmail.com.br>
2020-05-04 18:32:10 -0300

    Added test DCT code

--- src/main.c
+++ src/main.c
@@ -3,7 +3,9 @@
 
 int main(int argc, char *argv[])
 {
-	BMP_FILE *bmp = bmp_read_file("asd.bmp");
+	BMP_FILE *bmp = bmp_read_file("rainbowgirl.bmp");
+	bmp_dct(bmp, 0);
+	bmp_dct(bmp, -1);
 	bmp_write_file("teste.bmp", bmp);
 	bmp_destroy(&bmp);
 	return 0;
