07493c1068796dc1ea4c82c31a27cf2b5916c0b0
Paulo Ricard J. Miranda <pauloricardojm@hotmail.com.br>
2020-05-05 18:48:28 -0300

    Added test quantization and inverse quantization

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@ int main(int argc, char *argv[])
 {
 	BMP_FILE *bmp = bmp_read_file("samples/rainbowgirl.bmp");
 	bmp_dct(bmp, 0);
+	bmp_quantization(bmp);
+	bmp_inverse_quantization(bmp);
 	bmp_dct(bmp, -1);
 	bmp_write_file("samples/teste.bmp", bmp);
 	bmp_destroy(&bmp);
