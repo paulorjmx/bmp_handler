9ff02e1d19d88dcf8fbc771f219911046e8dfb82
Paulo Ricard J. Miranda <pauloricardojm@hotmail.com.br>
2020-05-05 18:46:22 -0300

    Declaration of quantization and inverse quantization

--- inc/bmp_handler.h
+++ inc/bmp_handler.h
@@ -7,6 +7,8 @@
 		BMP_FILE *bmp_read_file(const char *); // Read a BMP file and return the content stored
 		int bmp_write_file(const char *, BMP_FILE *); // Write a BMP file in the disk
 		void bmp_dct(BMP_FILE *, char); // Calculates the DCT-II 
+		void bmp_quantization(BMP_FILE *); // Apply the quantization in all channels
+		void bmp_inverse_quantization(BMP_FILE *); // Apply the inverse quantization in all channels
 		BMP_CHANNELS *bmp_get_channels();
 		void bmp_destroy(BMP_FILE **); // Free the memory used by BMP file 
 #endif
