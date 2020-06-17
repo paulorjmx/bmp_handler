d463dad82fa55aca36418d978da4e3680b4423ee
Paulo Ricardo J. Miranda <pauloricardojm@hotmail.com.br>
2020-06-17 19:55:27 -0300

    Implemented delta encoding and decoding

--- src/main.c
+++ src/main.c
@@ -3,12 +3,14 @@
 
 int main(int argc, char *argv[])
 {
-	BMP_FILE *bmp = bmp_read_file("samples/rainbowgirl.bmp");
+	BMP_FILE *bmp = bmp_read_file("lol.bmp");
 	bmp_dct(bmp, 0);
 	bmp_quantization(bmp);
+	bmp_diff_encode(bmp);
+	bmp_diff_decode(bmp);
 	bmp_inverse_quantization(bmp);
 	bmp_dct(bmp, -1);
-	bmp_write_file("samples/teste.bmp", bmp);
+	bmp_write_file("teste.bmp", bmp);
 	bmp_destroy(&bmp);
 	return 0;
 }
