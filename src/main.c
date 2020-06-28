39f9d3575a265e1d9f5189ee6b255497cd9d60d1
Paulo Ricard J. Miranda <pauloricardojm@hotmail.com.br>
2020-06-28 16:44:10 -0300

    Developing CLI interface

--- src/main.c
+++ src/main.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 #include <bmp_handler.h>
 
 int main(int argc, char *argv[])
 {
-	BMP_FILE *bmp = bmp_read_file("lol.bmp");
-	bmp_dct(bmp, 0);
-	bmp_quantization(bmp);
-	bmp_diff_encode(bmp);
-	bmp_diff_decode(bmp);
-	bmp_inverse_quantization(bmp);
-	bmp_dct(bmp, -1);
-	bmp_write_file("teste.bmp", bmp);
+	BMP_FILE *bmp = NULL;
+	if(strcmp(argv[1], "-c") == 0)
+	{
+		bmp = bmp_read_file("samples/rainbowgirl.bmp");
+		bmp_dct(bmp, 0);
+		bmp_quantization(bmp);
+		bmp_diff_encode(bmp);
+		// bmp_compute_huffman_code(bmp);
+		// bmp_compress(bmp, "teste.co");
+	}
+	else 
+	{
+		bmp = bmp_decompress("teste.co");
+		// bmp_diff_decode(bmp);
+		// bmp_inverse_quantization(bmp);
+		// bmp_dct(bmp, -1);
+		// bmp_write_file("saida.bmp", bmp);
+	}
 	bmp_destroy(&bmp);
 	return 0;
 }
