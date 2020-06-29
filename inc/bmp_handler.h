664a9197ea0b4b09dd927ad673dc32d5d8f0f9c2
Paulo Ricard J. Miranda <pauloricardojm@hotmail.com.br>
2020-06-28 21:29:32 -0300

    Removed huffman encode functions declarations

--- inc/bmp_handler.h
+++ inc/bmp_handler.h
@@ -11,8 +11,6 @@
 		void bmp_inverse_quantization(BMP_FILE *); // Apply the inverse quantization in all channels
 		void bmp_diff_encode(BMP_FILE *); // Calculate delta encoding for every image 8x8 block
 		void bmp_diff_decode(BMP_FILE *); // Decodes delta encoding for every image 8x8 block
-		void bmp_compute_huffman_code(BMP_FILE *); // Alloc blocks of encoded huffman code and encode all 8x8 blocks
-		void bmp_compute_inv_huffman_code(BMP_FILE *); //
 		void bmp_compress(BMP_FILE *, const char *); // Creates frame buffer and save file in a compressed format
 		BMP_FILE *bmp_decompress(const char *); // Decompress file compressed by bmp_compress
 		BMP_CHANNELS *bmp_get_channels();
